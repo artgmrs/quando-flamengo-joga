08e5ef8e85903e7bd665c26e24beaef6a2700ebc
Arthur Guimarães Rocha Miranda <c0639646@vale.com>
2023-02-11 18:52:42 -0300

    fix: correção na data gerada para o calendário

--- src/utils/GoogleCalendarUtils.js
+++ src/utils/GoogleCalendarUtils.js
@@ -5,13 +5,15 @@ const generateGoogleCalendarLink = ( nomeRival, date ) => {
   {
     const baseUrl = 'https://calendar.google.com/calendar/render';
   
-    const title = `Flamengo x ${nomeRival}`;
+    const title = encodeURIComponent(`Flamengo x ${nomeRival}`);
   
-    const startDate = date.toISOString().replace(/[^a-zA-Z0-9]/g, '');
-    const endDate = addHours(date, 2).toISOString().replace(/[^a-zA-Z0-9]/g, '');
+    const startDate = date.toISOString().replace(/[.]\d+/, '').replace(/[^a-zA-Z0-9]/g, '').replace('.000Z', 'Z');
+    const endDate = addHours(date, 2).toISOString().replace(/[.]\d+/, '').replace(/[^a-zA-Z0-9]/g, '').replace('.000Z', 'Z');
     const timeZone = 'America/Sao_Paulo';
   
-    const url = `${baseUrl}?action=TEMPLATE&text=${title}&dates=${startDate}/${endDate}&ctz=${timeZone}`
+    const url = `${baseUrl}?action=TEMPLATE&text=${title}&dates=${startDate}/${endDate}&ctz=${timeZone}`;
+
+    console.log(url);
   
     return url;
   }
